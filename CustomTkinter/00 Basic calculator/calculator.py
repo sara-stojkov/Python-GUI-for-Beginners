fa0ec7d4f6e4e125ce87c8a3ebc77d38be6e55ed
Sara Stojkov <150953122+sara-stojkov@users.noreply.github.com>
2024-03-26 09:25:43 +0100

    Add files via upload

---
+++ CustomTkinter/00 Basic calculator/calculator.py
@@ -0,0 +1,54 @@
+import customtkinter
+
+customtkinter.set_default_color_theme("dark-blue")
+customtkinter.set_appearance_mode("dark")
+
+
+def addition():
+    num1 = entry1.get()
+    num2 = entry2.get()
+    if num1 and num2:
+        sum = int(num1) + int(num2)
+        sum_label = customtkinter.CTkLabel(master=frame, text="Rezultat je " + str(sum))
+        sum_label.pack(padx=12, pady=10)
+
+
+def subtraction():
+    num1 = entry1.get()
+    num2 = entry2.get()
+    if num1 and num2:
+        sum = int(num1) - int(num2)
+        sum_label = customtkinter.CTkLabel(master=frame, text="Rezultat je " + str(sum))
+        sum_label.pack(padx=12, pady=10)
+
+root = customtkinter.CTk()  # kreiramo objekat
+root.geometry("800x600")
+
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(padx=12, pady=10)
+frame.configure(width=500)
+
+label = customtkinter.CTkLabel(master=frame, text="Calculator", font=("Arial", 20))
+label.pack(padx=12, pady=10)
+label.configure(width=400)
+
+entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="First Number", font=("Arial", 20))
+entry1.pack(padx=12, pady=10)
+entry1.configure(width=300)
+
+entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Second Number", font=("Arial", 20))
+entry2.pack(padx=12, pady=10)
+entry2.configure(width=400)
+
+button_plus = customtkinter.CTkButton(master=frame, text="+", command=addition, font=("Arial", 20))
+button_plus.pack(padx=12, pady=10)
+
+button_minus = customtkinter.CTkButton(master=frame, text="-", command=subtraction, font=("Arial", 20))
+button_minus.pack(padx=12, pady=10)
+
+root.mainloop()
+# button_minus = customtkinter.CTkButton(master=frame, text="-", command=subtraction(num1, num2))
+# button_minus.pack()
+
+
+# dodati button 1 za sabiranje, button2 za oduzimanje i da li label ili sta god za rezultat
